9ff2b3f598eb7a4cf920831f81b5a70eb583542c
Scott Ladd <scottjr101@gmail.com>
2020-01-03 16:58:09 -0500

    scraped r/worldnews

---
+++ server.js
@@ -0,0 +1,32 @@
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+// First, tell the console what server.js is doing
+console.log("\n***********************************\n" +
+            "Grabbing every thread name and link\n" +
+            "from reddit's world news board:" +
+            "\n***********************************\n");
+
+axios.get("https://old.reddit.com/r/worldnews/").then(function(response) {
+    var $ = cheerio.load(response.data);
+    var results = [];
+    $("p.title").each(function(i, element) {
+
+        // Save the text of the element in a "title" variable
+        var title = $(element).text();
+    
+        // In the currently selected element, look at its child elements (i.e., its a-tags),
+        // then save the values for any "href" attributes that the child elements may have
+        var link = $(element).children().attr("href");
+    
+        // Save these results in an object that we'll push into the results array we defined earlier
+        results.push({
+          title: title,
+          link: link
+        });
+      });
+    
+      // Log the results once you've looped through each of the elements found with cheerio
+      console.log(results);
+    
+});
