e50ab3f09815835b8dd516c08a5eec7188ddb542
Scott Ladd <scottjr101@gmail.com>
2020-01-06 16:16:36 -0500

    removed routes

--- server.js
+++ server.js
@@ -24,31 +24,7 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./routes/siteRoutes")(app);
-
-axios.get("https://old.reddit.com/r/worldnews/").then(function(response) {
-    var $ = cheerio.load(response.data);
-    var results = [];
-    $("p.title").each(function(index, element) {
-
-        // Save the text of the element in a "title" variable
-        var title = $(element).text();
-    
-        // In the currently selected element, look at its child elements (i.e., its a-tags),
-        // then save the values for any "href" attributes that the child elements may have
-        var link = $(element).children().attr("href");
-    
-        // Save these results in an object that we'll push into the results array we defined earlier
-        results.push({
-          title: title,
-          link: link
-        });
-      });
-    
-      // Log the results once you've looped through each of the elements found with cheerio
-      console.log(results);
-    
-});
+require("./routes/siteRoutes")(app, axios, cheerio);
 
 // Start the server
 app.listen(PORT, function() {
