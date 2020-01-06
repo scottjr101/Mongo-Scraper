7586eb4b59d8651ea9dfacc95ff608a62f58e05d
Scott Ladd <scottjr101@gmail.com>
2020-01-06 13:42:07 -0500

    Got handlebars added in and working

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
+var express = require("express");
 var cheerio = require("cheerio");
 var axios = require("axios");
+var exphbs = require("express-handlebars");
+var path = require("path");
 
-// First, tell the console what server.js is doing
-console.log("\n***********************************\n" +
-            "Grabbing every thread name and link\n" +
-            "from reddit's world news board:" +
-            "\n***********************************\n");
+var PORT = 5000;
+
+// Initialize Express
+var app = express();
+
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+      defaultLayout: "main",
+      partialsDir: [
+        //  path to your partials
+        path.join(__dirname, 'views/partials'),
+      ]
+    })
+  );
+app.set("view engine", "handlebars");
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/siteRoutes")(app);
 
 axios.get("https://old.reddit.com/r/worldnews/").then(function(response) {
     var $ = cheerio.load(response.data);
     var results = [];
-    $("p.title").each(function(i, element) {
+    $("p.title").each(function(index, element) {
 
         // Save the text of the element in a "title" variable
         var title = $(element).text();
@@ -30,3 +49,8 @@ axios.get("https://old.reddit.com/r/worldnews/").then(function(response) {
       console.log(results);
     
 });
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
