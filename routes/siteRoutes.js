a1cde5f773c9f42f844154bfbba24240a8bb4c57
Scott Ladd <scottjr101@gmail.com>
2020-01-09 14:01:29 -0500

    added route for saved notes

--- routes/siteRoutes.js
+++ routes/siteRoutes.js
@@ -96,30 +96,35 @@ module.exports = function (app, axios, cheerio) {
             });
     });
 
-    app.post("/articles/:id", function (req, res) {
-        // Create a new note and pass the req.body to the entry
-        db.Note.create(req.body)
-            .then(function (dbNote) {
-                // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-                // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-                // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-                return db.Article.findOneAndUpdate({
-                    _id: req.params.id
-                }, {
-                    note: dbNote._id
-                }, {
-                    new: true
-                });
-            })
-            .then(function (dbArticle) {
-                // If we were able to successfully update an Article, send it back to the client
-                res.json(dbArticle);
-            })
-            .catch(function (err) {
-                // If an error occurred, send it to the client
-                res.json(err);
-            });
-    });
+    // Route for saving/updating an Article's associated Note
+    app.post("/save-note/:id", function(req, res) {
+	console.log("saving / updating note route - req.params.id: " + req.params.id);
+	console.log("saving / updating note route - req.body: " + JSON.stringify(req.body));
+	
+	// save the new note that gets posted to the Notes collection
+	// then find an article from the req.params.id
+	// and update it's "note" property with the _id of the new note
+	db.Note.create(req.body)
+		.then(function(dbNote) {
+
+			return db.Article.findOneAndUpdate({
+				_id: req.params.id
+			}, {
+				$push: {
+					note: dbNote._id
+				}}, {
+				new: true
+			}).populate("note");
+		})
+		// If the Article was updated successfully, send back article and its corresponding notes to the client
+		.then(function(dbArticle){
+			console.log("dbArticle with notes" + dbArticle);
+			res.json({success: true});
+		})
+		.catch(function(err) {
+			res.writeContinue(err);
+		});
+});
 
     app.post("/saved/:id", function (req, res) {
         // Create a new note and pass the req.body to the entry
